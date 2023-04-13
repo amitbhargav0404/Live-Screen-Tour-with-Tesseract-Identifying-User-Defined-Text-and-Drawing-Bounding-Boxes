5bf775556ae8739d035efcfbb3e409197ca63477
amitbhargav0404 <129143150+amitbhargav0404@users.noreply.github.com>
2023-04-13 12:34:07 +0530

    Update image_classification_live.py
    code update

--- image_classification_live.py
+++ image_classification_live.py
@@ -34,18 +34,7 @@ def show_message(screen, live_text_filtered,org,font_scale=1):
     # Show the screen with the message
     cv2.imshow("Screen", screen)
     cv2.waitKey(1)
-    # screen = np.array(screen)
-    # # Calculate the size of the message box based on the length of the text
-    # (w, h), _ = cv2.getTextSize("Welcome to my program!", font, fontScale=font_scale, thickness=1)
-    # x = int(screen.shape[1]/2 - w/2)
-    # y = int(screen.shape[0]/2 - h/2)
-    # # Draw a white rectangle as the background of the message box
-    # cv2.rectangle(screen, (x-2, y-2), (x+w+10, y+h+10), (255, 255, 255), -1)
-    # # Draw the text on the message box
-    # cv2.putText(screen, "Welcome to my program!", (x, y+h), font, fontScale=font_scale, color=(0, 0, 0), thickness=0, lineType=cv2.LINE_AA)
-    # # Show the screen with the message
-    # cv2.imshow("Screen", screen)
-    # cv2.waitKey(1)
+ 
 
 
 def main():
@@ -69,13 +58,8 @@ def main():
             show_message(screen, "Hello, World!",org=org,font_scale=0.5)
             # Wait for a short time to avoid overwhelming the CPU
             print("value come here ?:----")
-            
-            
             cv2.waitKey(100)
         else:
-            # Clear the message box and continue capturing the screen
-            # cv2.imshow("User Guided Screen", screen)
-            # cv2.waitKey(1)
             print("value come here:-----")
         # Check if the 'q' key is pressed to quit the program
         if cv2.waitKey(1) & 0xFF == ord('q'):
